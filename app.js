893e6da6a4f5f698e9e30e0534be262d9d2863ce
alexisraimbault <27raimbault.alexis@gmail.com>
2021-09-05 19:52:14 +0200

    changed basic stuff

--- app.js
+++ app.js
@@ -20,17 +20,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/session', sessionRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.js
+++ routes/users.js
@@ -5,47 +5,47 @@ const { generateAccessToken, authenticateToken } = require('../middlewares/authe
 const { query } = require('../query/query.js');
 
 /* Creating a new user. */
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   const { firstname, lastname, email, password } = req.body;
 
   query(
     'INSERT INTO public."User"(firstname, lastname, email, password) values ($1, $2, $3, $4) RETURNING id',
     [firstname, lastname, email, password]
-    ).then(queryResponse => {
-      const token = generateAccessToken({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        id: queryResponse.rows[0].id,
-      });
+  ).then(queryResponse => {
+    const token = generateAccessToken({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      id: queryResponse.rows[0].id,
+    });
 
-      res.json({token, firstname, lastname, email, id: queryResponse.rows[0].id});
-    }).catch(err => {
-      res.json({err});
-    })
+    res.json({ token, firstname, lastname, email, id: queryResponse.rows[0].id });
+  }).catch(err => {
+    res.json({ err });
+  })
 });
 
 /* Login to my account. */
-router.get('/login', function(req, res, next) {
-  const { email, password } = req.body;
+router.get('/login', function (req, res, next) {
+  const { email, password } = req.query;
 
   query(
     `SELECT * FROM public."User" where email='${email}' and password='${password}'`,
     []
-    ).then(queryResponse => {
-      const {firstname, lastname, id, email} = queryResponse.rows[0];
+  ).then(queryResponse => {
+    const { firstname, lastname, id, email } = queryResponse.rows[0];
 
-      const token = generateAccessToken({
-        firstname,
-        lastname,
-        email,
-        id,
-      });
+    const token = generateAccessToken({
+      firstname,
+      lastname,
+      email,
+      id,
+    });
 
-      res.json({token, firstname, lastname, email, id});
-    }).catch(err => {
-      res.json({err});
-    })
+    res.json({ token, firstname, lastname, email, id });
+  }).catch(err => {
+    res.json({ err });
+  })
 });
 
 module.exports = router;
